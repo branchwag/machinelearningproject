ddd3ed94669b17a01051d6259e23ecc15c816b19
LeAnne B <branchleannelmb@gmail.com>
2024-04-15 09:26:55 -0400

    inputs groundwork for drawing paths

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -11,14 +11,38 @@ class SketchPad {
 
     this.ctx = this.canvas.getContext("2d");
 
+    this.path = [];
+    this.isDrawing = false;
+
     this.#addEventListeners();
   }
 
   #addEventListeners() {
     this.canvas.onmousedown = (evt) => {
-      const rect = this.canvas.getBoundingClientRect();
-      const mouse = [evt.clientX - rect.left, evt.clientY - rect.top];
-      console.log(mouse);
+      const mouse = this.#getMouse(evt);
+      //console.log(mouse);
+      this.path = [mouse];
+      this.isDrawing = true;
+    };
+
+    this.canvas.onmousemove = (evt) => {
+      if (this.isDrawing) {
+        const mouse = this.#getMouse(evt);
+        this.path.push(mouse);
+        console.log(this.path.length);
+      }
+    };
+
+    this.canvas.onmouseup = () => {
+      this.isDrawing = false;
     };
   }
+
+  #getMouse = (evt) => {
+    const rect = this.canvas.getBoundingClientRect();
+    return [
+      Math.round(evt.clientX - rect.left),
+      Math.round(evt.clientY - rect.top),
+    ];
+  };
 }
