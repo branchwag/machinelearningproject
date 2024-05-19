798a06c347acef7c2ee7ea839ff72373e1fa5524
LeAnne B <branchleannelmb@gmail.com>
2024-05-19 10:14:10 -0400

    adjusts scrolling to item

--- web/js/display.js
+++ web/js/display.js
@@ -13,6 +13,7 @@ function createRow(container, studentName, samples) {
 
     const sampleContainer = document.createElement("div");
     sampleContainer.id = "sample_" + id;
+    sampleContainer.onclick = () => handleClick(sample, false);
     sampleContainer.classList.add("sampleContainer");
 
     const sampleLabel = document.createElement("div");
@@ -30,14 +31,16 @@ function createRow(container, studentName, samples) {
   }
 }
 
-function handleClick(sample) {
+function handleClick(sample, doScroll = true) {
   [...document.querySelectorAll(".emphasize")].forEach((e) =>
     e.classList.remove("emphasize")
   );
   const el = document.getElementById("sample_" + sample.id);
   el.classList.add("emphasize");
-  el.scrollIntoView({
-    behavior: "auto",
-    block: "center",
-  });
+  if (doScroll) {
+    el.scrollIntoView({
+      behavior: "auto",
+      block: "center",
+    });
+  }
 }
